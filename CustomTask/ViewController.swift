00e5d835f2295a0635cf1771257a1b08d162a342
Jhansi <jhansidevi.chundru@narayanagroup.com>
2022-09-12 14:03:06 +0530

    cleared comments in viewcontroller

--- CustomTask/ViewController.swift
+++ CustomTask/ViewController.swift
@@ -10,21 +10,12 @@ import UIKit
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
-//    let questions = [0,10,0,80,10]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        if questions.sum() == 100 {
-//            studentsMarksRecords.dataEntries = [QuestionsRecord(value: questions[0], label: " \(questions[0]) Correct Answers", colour: .systemGreen),QuestionsRecord(value: questions[1], label: " \(questions[1]) Skipped Visited", colour: .systemOrange),QuestionsRecord(value: questions[2], label: " \(questions[2]) Partially Correct", colour: .systemCyan),QuestionsRecord(value: questions[3], label: " \(questions[3]) Skipped Not Visited", colour: .systemYellow),QuestionsRecord(value: questions[4], label: " \(questions[4]) InCorrect  Answers", colour: .systemRed)]
-//        }
-//        else {
-//            print("Total number of Questions must be 100")
-//        }
+
         tableView.dataSource = self
         tableView.delegate = self
-//        view.addSubview(tableView)
-//        tableView.addSubview(studentsMarksRecords)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -32,7 +23,6 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
-//        cell.studentsRecords.addSubview(StudentsMarksRecords(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 160)))
         return cell
     }
 
